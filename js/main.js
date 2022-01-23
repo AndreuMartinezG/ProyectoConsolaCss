060e5550a99ab84c0711ebaf2a49bfb10caeb41d
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-01-23 17:44:50 +0100

    Añadidos Assests de sonido y video, añadido boton ON/OFF y configurado en el JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+var PowerSwitch = document.getElementById('power');
+var ScreenMode = document.getElementById('pantalla-cent-cent');
+const musicGB = new Audio('assets/sounds/gbStart.mp3');
+const musicPoke = new Audio('assets/sounds/pokeOpen.mp3')
+
+function powerOn() {
+    if (PowerSwitch.style.backgroundColor == "black") {
+        document.body.style.backgroundColor = "grey";
+        PowerSwitch.style.backgroundColor = "Red";
+        musicGB.volume = 0.8;
+        musicGB.play();
+        ScreenMode.style.border = "1px solid grey";
+        ScreenMode.style.boxSizing = "border-box";
+        ScreenMode.style.backgroundImage = "url('assets/img/GBColor-start.gif')";
+        setTimeout(function() {
+            musicPoke.volume = 0.2;
+            musicPoke.play();
+            ScreenMode.style.backgroundImage = "url('assets/img/pokemon-start.gif')";
+        }, 4000);
+    }else {
+        document.body.style.backgroundColor = "rgb(233, 233, 201)";
+        musicGB.pause();
+        musicPoke.pause();
+        PowerSwitch.style.backgroundColor = "black";
+        ScreenMode.style.backgroundImage = "none";
+        ScreenMode.style.backgroundColor = "grey";
+    }
+}
+
 
